abbbb804ea6e9b0a65fd9aa809d1f099dbdcc8e9
Thales Mariano <thales.mariano125@gmail.com>
2024-10-05 14:18:03 -0300

    Cart icon added to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,13 +3,16 @@ import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-    <header class="p-5">
+    <header class="w-full flex justify-between items-center p-5">
+        <div></div>
         <h1 class="text-center font-bold text-3xl text-red-500">
             <RouterLink to="/">
                 Pizzas
                 Delivery
             </RouterLink>
-
         </h1>
+        <button>
+            <img class="w-7 mt-1" src="@/assets/icons/cart2.svg" />
+        </button>
     </header>
 </template>
